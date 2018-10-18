04d56d529cb62504939787de450bab6321cf3c8b
jocelynnnn <465497055@qq.com>
2018-10-18 22:54:23 +0800

    Task5

--- Offer/src/Task5.java
+++ Offer/src/Task5.java
@@ -2,7 +2,6 @@
 //注意：在原字符串上进行替换，并且保证输入的字符串后面有足够多的空余内存。
 
 public class Task5 {
-    //length 为总长度
     public void ReplaceBlank(String s){
         //首先遍历，找到所有空格
         int count0 = 0;
@@ -32,13 +31,40 @@ public class Task5 {
             }
         }
         System.out.print(current_c);
-
-
     }
+    //参数为StringBuffer，对象可以被多次修改，且不产生新对象
+    public void ReplaceBlank_2(StringBuffer s){
+        int p1 = s.length()-1;
+        for (int i=0; i<p1; i++){
+            if (s.charAt(i)==' '){
+               s.append("  ");  //添加两个空格
+            }
+        }
+        int p2 = s.length()-1;
+        while(p1!=p2 && p1>=0){
+            char c = s.charAt(p1);
+            if (c!=' '){
+                s.setCharAt(p2,c);
+                p1--;
+                p2--;
+            }
+            else{
+                s.setCharAt(p2,'0'); p2--;
+                s.setCharAt(p2,'2'); p2--;
+                s.setCharAt(p2,'%'); p2--;
+                p1--;
+            }
+        }
+        System.out.print(s);
+    }
+
+
+
     public static void main(String[] args) {
         String s = "we are young!";
+        StringBuffer s_2 = new StringBuffer("we are young!");
         Task5 t_5 = new Task5();
         t_5.ReplaceBlank(s);
-
+        t_5.ReplaceBlank_2(s_2);
     }
 }
