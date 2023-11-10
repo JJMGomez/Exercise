3c092fcdccaa5beee70d615ee00e1c68064213e4
jocelynnnn <465497055@qq.com>
2023-11-10 12:50:31 -0500

    google foo level2 task2 —— En Route Salute

---
+++ foo/src/EnRouteSalute/Solution.java
@@ -0,0 +1,41 @@
+package EnRouteSalute;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static int solution(String s){
+        Map<Integer,Integer> resultMap = new HashMap<>();
+        ArrayList<Integer> directionList = new ArrayList<>();
+        for (char a: s.toCharArray()){
+            if ('>' == a){
+                directionList.add(0);
+            }else if ('<' == a){
+                directionList.add(1);
+            }
+        }
+        for (int i=0; i<directionList.size(); i++){
+            resultMap.put(i,0);
+        }
+        for (int i=0; i<directionList.size(); i++){
+            if (1 == directionList.get(i)){
+                for (int j=0; j<i; j++){
+                    if (0 == directionList.get(j)){
+                        resultMap.put(j,resultMap.get(j) + 1);
+                        resultMap.put(i,resultMap.get(i) + 1);
+                    }
+                }
+            }
+        }
+        int result = 0;
+        for (int i=0; i<resultMap.size(); i++){
+            result += resultMap.get(i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("<<>><"));
+    }
+}
