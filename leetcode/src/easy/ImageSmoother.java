ecb1645e02ef85fc305d9d4fad7642b80dcc7b11
Jocelyn <jocelyn.j.m.gomez@gmail.com>
2023-12-19 11:46:38 -0500

    Leetcode Daily Problem 661

---
+++ leetcode/src/easy/ImageSmoother.java
@@ -0,0 +1,44 @@
+package easy;
+
+public class ImageSmoother {
+    public int n;
+    public int m;
+    public int[][] image;
+
+    public int count=0;
+    public int[][] imageSmoother(int[][] img) {
+        m = img.length;
+        n = img[0].length;
+        image = img;
+        int [][] res = new int[m][n];
+        for (int i=0; i<m; i++){
+            for (int j=0; j<n; j++){
+                count=0;
+                res[i][j] = (getPoint(i-1,j-1)
+                        + getPoint(i,j-1)
+                        + getPoint(i+1,j-1)
+                        + getPoint(i-1,j)
+                        + getPoint(i-1,j+1)
+                        + getPoint(i,j+1)
+                        + getPoint(i+1, j)
+                        + getPoint(i+1,j+1)
+                        + getPoint(i,j))/count;
+            }
+        }
+        return res;
+    }
+
+    public int getPoint(int i,int j){
+        if (i<0 || i>=m || j<0 || j>=n){
+            return 0;
+        }
+        count++;
+        return image[i][j];
+    }
+
+    public static void main(String[] args) {
+        int[][] img_case1 = new int[][]{{100,200,100},{200,50,200},{100,200,100}};
+        ImageSmoother imageSmoother = new ImageSmoother();
+        imageSmoother.imageSmoother(img_case1);
+    }
+}
